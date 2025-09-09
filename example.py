9d20dca39bfddac4e4a894de9ea0a155d0e03d59
梁润铎 <loyalaujust@outlook.com>
2025-09-09 23:16:16 +0800

    update

--- example.py
+++ example.py
@@ -1,3 +1,7 @@
 import SOCS_Xray
+from astropy.table import Table
 
-SOCS_Xray.fetch.get_TNS(save_dir='./data')
+#SOCS_Xray.fetch.get_TNS(save_dir='./data')
+#SOCS_Xray.fetch.update_WXT_source_list('liangrd@bao.ac.cn','Liang981127','./data')
+
+data = Table.read('/Volumes/T7/Shared_Files/EP/Projects/SOCS-Xray/data/tns_public_objects.csv.zip')
