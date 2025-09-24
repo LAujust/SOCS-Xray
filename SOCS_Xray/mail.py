87a362d651370ef487353e4198475ac147287cf2
梁润铎 <loyalaujust@outlook.com>
2025-09-24 13:41:12 +0800

    update

--- SOCS_Xray/mail.py
+++ SOCS_Xray/mail.py
@@ -17,15 +17,15 @@ def send_email(smtp_server,smtp_port,sender_email,receiver_emails,password,html_
     msg["To"] = ", ".join(receiver_emails)
     msg["Subject"] = title
     
-    try:
-        with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_emails, msg.as_string())
-    except:
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_emails, html_body)
+    # try:
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_emails, msg.as_string())
+    # except:
+    #     with smtplib.SMTP(smtp_server, smtp_port) as server:
+    #         server.starttls()
+    #         server.login(sender_email, password)
+    #         server.sendmail(sender_email, receiver_emails, html_body)
 
     print("✅ HTML email sent!")
     
