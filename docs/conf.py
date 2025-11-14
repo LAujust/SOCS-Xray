efdd0062a41add47c1f43ac744d60d1aab1257c2
梁润铎 <loyalaujust@outlook.com>
2025-11-14 23:57:09 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'R. D. Liang'
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
