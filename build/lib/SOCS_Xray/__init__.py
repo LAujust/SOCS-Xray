2e1fc7690afad2190612152291b4eb7c8b3d15f7
LAujust <loyalaujust@outlook.com>
2026-03-06 07:06:49 +0000

    update

---
+++ build/lib/SOCS_Xray/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1"
+
+from .utils import *
+from .fetch import *
+from .search import *
+from .pipeline import *
+from .mail import *
+from .lightcurve import *
---
+++ build/lib/SOCS_Xray/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from astropy.table import Table, vstack, hstack, setdiff, unique
+from astropy.io.votable import parse_single_table
+from astropy.time import Time
+import astropy.units as u
+import astropy.constants as c
+from astropy.coordinates import SkyCoord
+import requests
+import sys, os, re
+import subprocess
+import math
+import yaml
+from tqdm import tqdm
+import time
